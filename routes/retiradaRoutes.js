a91defcb05b2181bee4f36ed3d2194172417647b
Lukntc84 <lauro.os84@gmail.com>
2026-07-31 16:09:48 -0300

    Adiciona perfil visualizador

--- routes/retiradaRoutes.js
+++ routes/retiradaRoutes.js
@@ -11,17 +11,33 @@ function exigeLogin(req, res, next) {
     next();
 }
 
+function podeCriarRetirada(req, res, next) {
+    const usuario = req.session.usuario;
+
+    if (
+        usuario.tipo !== "admin" &&
+        usuario.tipo !== "fotografia" &&
+        usuario.tipo !== "retirada"
+    ) {
+        return res.status(403).send("Você não tem permissão para criar retirada.");
+    }
+
+    next();
+}
+
 router.get("/dashboard", exigeLogin, retiradaController.dashboard);
 
 router.get(
     "/nova-retirada",
     exigeLogin,
+    podeCriarRetirada,
     retiradaController.telaNovaRetirada
 );
 
 router.post(
     "/nova-retirada",
     exigeLogin,
+    podeCriarRetirada,
     retiradaController.salvarRetirada
 );
 
